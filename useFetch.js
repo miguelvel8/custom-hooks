9e39e5efa86704283ce5dffeb354943cbed17443
miguelvel8 <miguelvel8>
2021-03-17 16:21:30 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState, useRef } from "react"
+
+export const useFetch = (url) => {
+    
+    const isMounted = useRef(true);
+    const [state, setState] = useState({data: null, loading: true, error:null})
+
+    useEffect(() => {
+        return () => {
+            isMounted.current= false;
+        }
+    }, [] ) // cuando se renderiza por primera vez 
+    
+    useEffect(() => {
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                
+                if(isMounted.current){
+                    
+                    setState({
+                        data,
+                        loading:false,
+                        error:null
+                    })
+                }
+                
+            })
+
+    }, [url])
+
+    return state;
+
+}
